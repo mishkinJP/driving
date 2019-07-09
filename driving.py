341a5b3d48d7496a14275f5e086fd808e3106e7c
Mishkin <zq@jr360.com>
2019-07-09 14:37:10 +0800

    add us part

--- driving.py
+++ driving.py
@@ -1,8 +1,13 @@
 coutry = input('请问你是哪国人: ')
-age = input('请输入你的年龄')
+age = input('请输入你的年龄:')
 age =int(age)
 if coutry == '台湾':
 	if age >= 18:
 		print('你能考驾照')
 	else:
 		print('你还不能开车')
+if coutry == '美国':
+	if age >= 16:
+		print('你可以考驾照')
+	else:
+		print('你还不能考驾照')
